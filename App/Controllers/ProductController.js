bf0e9338af7f513f67dc1e5b157a9fa15b922507
minzhoo2206-alt <minzhoo2206@gmail.com>
2026-09-24 02:35:51 +0700

    giaodien

--- App/Controllers/ProductController.js
+++ App/Controllers/ProductController.js
@@ -5,26 +5,29 @@ fetch(api)
         let container = document.querySelector('#productContainer');
 
         data.forEach(products => {
+            let col = document.createElement('div');
+            col.className = "col"
+
             let card = document.createElement('div');
-            card.className = "card m-2";
-            card.style.with = "18rem";
+            card.className = "card h-100 shadow-sm";
 
             let cardBody = document.createElement('div');
-            cardBody.className = "card-body";
+            cardBody.className = "card-body text-center";
 
             let h5 = document.createElement('h5');
-            h5.className = "card-title"
+            h5.className = "card-title text-break-anywhere"
             h5.textContent = products.name
             cardBody.appendChild(h5);
 
             let btn = document.createElement('a');
-            btn.className = "btn btn-primary";
+            btn.className = "btn btn-sm btn-info w-100 mt-2";
             btn.href = "#";
             btn.textContent = "Xem chi tiet";
             cardBody.appendChild(btn);
             
             card.appendChild(cardBody);
-            container.appendChild(card);
+            col.appendChild(card);
+            container.appendChild(col);
         });
         return console.log(data);
     })
