90f2f4eca1ff37609d3e1fb66171ab68ea6f6ecc
minzhoo2206-alt <minzhoo2206@gmail.com>
2026-09-24 10:24:48 +0700

    update

--- App/Controllers/ProductController.js
+++ App/Controllers/ProductController.js
@@ -1,34 +1,44 @@
-let api ="http://localhost:3000/products";
+let api = "http://localhost:3000/products";
 fetch(api)
-    .then(Response=>Response.json())
-    .then(function(data){
-        let container = document.querySelector('#productContainer');
+  .then((Response) => Response.json())
+  .then(function (data) {
+    let container = document.querySelector("#productContainer");
 
-        data.forEach(products => {
-            let col = document.createElement('div');
-            col.className = "col"
+    data.forEach((products) => {
+      let col = document.createElement("div");
+      col.className = "col";
 
-            let card = document.createElement('div');
-            card.className = "card h-100 shadow-sm";
+      let card = document.createElement("div");
+      card.className = "card h-100 shadow-sm";
 
-            let cardBody = document.createElement('div');
-            cardBody.className = "card-body text-center";
+      let img = document.createElement("img");
+      img.className = "card-img-top product-card-img";
+      img.src = products.image;
+      img.alt = products.name;
+      card.appendChild(img);
 
-            let h5 = document.createElement('h5');
-            h5.className = "card-title text-break-anywhere"
-            h5.textContent = products.name
-            cardBody.appendChild(h5);
+      let cardBody = document.createElement("div");
+      cardBody.className = "card-body text-center";
 
-            let btn = document.createElement('a');
-            btn.className = "btn btn-sm btn-info w-100 mt-2";
-            btn.href = "#";
-            btn.textContent = "Xem chi tiet";
-            cardBody.appendChild(btn);
-            
-            card.appendChild(cardBody);
-            col.appendChild(card);
-            container.appendChild(col);
-        });
-        return console.log(data);
-    })
-    .catch(error => console.error('Error;', error));
+      let h5 = document.createElement("h5");
+      h5.className = "card-title text-break-anywhere";
+      h5.textContent = products.name;
+      cardBody.appendChild(h5);
+
+      let detail = document.createElement("p");
+      detail.textContent = products.detail;
+      cardBody.appendChild(detail);
+
+      let btn = document.createElement("a");
+      btn.className = "btn btn-sm btn-info w-100 mt-2";
+      btn.href = "#";
+      btn.textContent = "Xem chi tiet";
+      cardBody.appendChild(btn);
+
+      card.appendChild(cardBody);
+      col.appendChild(card);
+      container.appendChild(col);
+    });
+    return console.log(data);
+  })
+  .catch((error) => console.error("Error;", error));
